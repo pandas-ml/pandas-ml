8740bf537491df604188d87f74750512e68f6f02
sinhrks <sinhrks@gmail.com>
2015-10-24 16:14:29 +0900

    ENH: Add plotting function

--- pandas_ml/core/frame.py
+++ pandas_ml/core/frame.py
@@ -477,6 +477,20 @@ class ModelFrame(pd.DataFrame, ModelPredictor):
             warnings.warn(msg.format(estimator.__class__.__name__))
         return probability
 
+    def plot_estimator(self):
+        import pandas_ml.core.base as base
+        if isinstance(self.estimator, base._ClassifierMixin):
+            from pandas_ml.plotting import ClassifierPlotter
+            return ClassifierPlotter(self, self.estimator).plot()
+        elif isinstance(self.estimator, base._RegressorMixin):
+            from pandas_ml.plotting import RegressorPlotter
+            return RegressorPlotter(self, self.estimator).plot()
+        elif self.estimator is None:
+            raise ValueError("Unable to plot estimator because it is not set")
+        else:
+            msg = '{0} is not supported'.format(self.estimator.__class__.__name__)
+            raise NotImplementedError(msg)
+
     @Appender(_shared_docs['estimator_methods'] %
               dict(funcname='score', returned='returned : score'))
     def score(self, estimator, *args, **kwargs):
---
+++ pandas_ml/plotting/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+from pandas_ml.plotting.estimator import ClassifierPlotter, RegressorPlotter
