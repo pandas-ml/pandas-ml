c08a6063016ba5b8ada54624f1b7e4feecaaca90
sinhrks <sinhrks@gmail.com>
2016-10-07 07:42:50 +0900

    API: Add confusion matrix related to top namespace

--- pandas_ml/__init__.py
+++ pandas_ml/__init__.py
@@ -4,3 +4,7 @@ from pandas_ml.core import ModelFrame, ModelSeries       # noqa
 from pandas_ml.tools import info                         # noqa
 from pandas_ml.version import version as __version__     # noqa
 import pandas_ml.compat as compat                        # noqa
+
+from pandas_ml.confusion_matrix.utils import (TRUE_NAME_DEFAULT, PRED_NAME_DEFAULT, Backend)  # noqa
+from pandas_ml.confusion_matrix.cm import LabeledConfusionMatrix, ConfusionMatrix  # noqa
+from pandas_ml.confusion_matrix.bcm import BinaryConfusionMatrix  # noqa
