11b265073f26b5aaa2a54e38bb2a0d0a0e87bf84
Sinhrks <sinhrks@gmail.com>
2019-01-15 11:25:32 +0900

    REL: v0.6.0 (#114)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ PACKAGE = 'pandas_ml'
 README = 'README.rst'
 REQUIREMENTS = 'requirements.txt'
 
-VERSION = '0.6.0.dev'
+VERSION = '0.6.0'
 
 def read(fname):
   # file must be read as utf-8 in py3 to avoid to be bytes
